223eaedce007ffe212e323e3d2fb479fe03a7363
JYOTY BHUSHAN SINHA <hunnysinha127@gmail.com>
2024-05-16 14:43:23 +0530

    all cahnges done

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
+
+
 const wrapper = document.querySelector('.wrapper');
 const question = document.querySelector('.question');
 const yesBtn = document.querySelector('.yes-btn');
 const noBtn = document.querySelector('.no-btn');
+const muteBtn = document.getElementById('mute-btn');
+const video = document.getElementById('bg-video');
 const wrapperRect = wrapper.getBoundingClientRect();
 const noBtnRect = noBtn.getBoundingClientRect();
+
 yesBtn.addEventListener('click', () => {
-    question.innerHTML = 'Then Send me Video of yours';
+    question.innerHTML = `Then send me video of your's :)`;
 });
+
 noBtn.addEventListener('mouseover', () => {
     const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
     const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
     noBtn.style.left = i + 'px';
     noBtn.style.top = j + 'px';
+});
+
+muteBtn.addEventListener('click', () => {
+    if (video.muted) {
+        video.muted = false;
+        muteBtn.textContent = 'Mute';
+    } else {
+        video.muted = true;
+        muteBtn.textContent = 'Unmute';
+    }
 });
